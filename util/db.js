953dbeb781ea692fe775a92b2d0a0976c6526b28
yael.lotan <yaelotan19@gmail.com>
2020-05-04 12:12:50 +0300

    fix(util/db.js): use env varibales instead of hardcoding varibales

--- util/db.js
+++ util/db.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 function init() {
     const { Client } = require('pg');
     const client = new Client({
-      user: 'yael',
-      database: 'gifts',
-      password: 'ct,h kvmkhj',
+      user: process.env.DB_USER,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
     });
   
     client.connect().then(console.log('gift DB is connected!'));
