21fff68476e4e7d1dae22f9ac6cf0ce473dc1609
yael.lotan <yaelotan19@gmail.com>
2020-05-11 17:53:49 +0300

    fix(package.json): scripts

--- util/db.js
+++ util/db.js
@@ -18,18 +18,4 @@ const init = () => {
 
 
 module.exports = init;
-
-// function init() {
-//     const { Client } = require('pg');
-//     const client = new Client({
-//       user: process.env.DB_USER,
-//       database: process.env.DB_DATABASE,
-//       password: process.env.DB_PASSWORD,
-//     });
-  
-//     client.connect().then(console.log('gift DB is connected!'));
-//     return client;
-//   };
-  
-//   module.exports = init;
   
